849e1802eb38ff8b09d2f72ebfe9e0c5684a35df
Gwendoline Sanabria <gwensanabria@gmail.com>
2020-05-09 22:06:35 -0500

    pull elements from HTML

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,30 @@
+// Elements
+const passwordEl = document.getElementById('password');
+const lengthEl = document.getElementById('length');
+const lowercaseEl = document.getElementById('lowercase');
+const uppercaseEl = document.getElementById('uppercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+
+
+
+const masterFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+
+generateEl.addEventListener('click', () => {
+  event.preventDefault;
+  const length = lengthEl.Value;
+
+console.log(length);
+
+})
+
+
 // Generate Function
 
 /* function randomElement (array) {
@@ -7,27 +34,23 @@
 function getRandomLower () {
   const lower = 'abcdefghijklmnopqrstuvwxyz'.split('');
   return lower[Math.floor(Math.random() * lower.length)];
-  
 }
 console.log(getRandomLower());
 
 function getRandomUpper () {
   const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
   return upper[Math.floor(Math.random() * upper.length)];
-  
 }
 console.log(getRandomUpper());
 
 function getRandomNumber () {
   const numbers = '0123456789'.split('');
   return numbers[Math.floor(Math.random() * numbers.length)];
-  
 }
 console.log(getRandomNumber());
 
 function getRandomSymbol () {
   const symbols = ' !@#$%^&*(){}[]=<>/,.?'.split('');;
   return symbols[Math.floor(Math.random() * symbols.length)];
-  
 }
 console.log(getRandomSymbol());
