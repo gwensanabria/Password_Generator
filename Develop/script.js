2491db6e8f7b4ec85e6bc13c63558c069a90bc0e
Gwendoline Sanabria <gwensanabria@gmail.com>
2020-05-09 21:53:41 -0500

    added functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,37 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Generate Function
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+/* function randomElement (array) {
+  return array[Math.floor(Math.random() * array.length)]
+} */
 
-  passwordText.value = password;
-
+function getRandomLower () {
+  const lowers = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const lower = lowers[Math.floor(Math.random() * lowers.length)];
+  
+  return lower;
 }
+console.log(getRandomLower());
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function getRandomUpper () {
+  const uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  const upper = uppers[Math.floor(Math.random() * uppers.length)];
+  
+  return upper;
+}
+console.log(getRandomUpper());
+
+function getRandomNumber () {
+  const numbers = '0123456789'.split('');
+  const number = numbers[Math.floor(Math.random() * numbers.length)];
+  
+  return number;
+}
+console.log(getRandomNumber());
+
+function getRandomSymbol () {
+  const symbols = ' !@#$%^&*(){}[]=<>/,.?'.split('');;
+  const symbol = symbols[Math.floor(Math.random() * symbols.length)];
+  
+  return symbol;
+}
+console.log(getRandomSymbol());
